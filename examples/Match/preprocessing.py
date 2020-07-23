eb8f1ac934756ebb3f33a9ed14a70a154229ed05
danyang <13673370381@163.com>
2020-07-23 22:17:11 +0800

    # add char embedding

--- examples/Match/preprocessing.py
+++ examples/Match/preprocessing.py
@@ -20,19 +20,21 @@ import source.utils.Constant as constants
 from source.inputters.field import TextField, NumberField
 
 logger = init_logger()
-user_dict_name = "/home/gong/zz/data/Match/dict_all.txt"
+user_dict_name = "/home/administrator4/ZDY/dataset/similarity/dict_all.txt"
 logger.info("loading {} user_dict".format(user_dict_name))
 jieba.load_userdict(user_dict_name)
 
-stopwords_file = "/home/gong/zz/data/Match/stop_words.txt"
+stopwords_file = "/home/administrator4/ZDY/dataset/similarity/stop_words.txt"
 logger.info("loading {} stop word".format(stopwords_file))
 stopwords = {line.strip(): 0 for line in open(stopwords_file, 'r', encoding="utf-8").readlines()}
 
 
 class Example(object):
-    def __init__(self, text_a, text_b, label):
+    def __init__(self, text_a, text_b, char_a, char_b, label):
         self.text_a = text_a
         self.text_b = text_b
+        self.char_a = char_a,
+        self.char_b = char_b,
         self.label = label
 
 
@@ -40,18 +42,26 @@ class InputFeatures(object):
     """A single set of features of data."""
 
     def __init__(self,
-                 left_ids, right_ids, left_len, right_len, label):
+                 left_ids, right_ids, left_char_ids, right_char_ids, left_len, right_len, left_chars_len, right_chars_len, label):
         self.left_ids = left_ids
         self.right_ids = right_ids
+        self.left_char_ids = left_char_ids
+        self.right_char_ids = right_char_ids
         self.left_len = left_len
         self.right_len = right_len
+        self.left_chars_len = left_chars_len
+        self.right_chars_len = right_chars_len
         self.label = label
+
+
+
 
 
 class MatchCorpus(object):
     def __init__(self,
                  args,
                  max_vocab_size=50000,
+                 max_char_size=50000,
                  min_freq=1,
                  specials=None):
         super(MatchCorpus, self).__init__()
@@ -59,14 +69,17 @@ class MatchCorpus(object):
         self.args = args
         self.data_file = os.path.join(args.data_dir, "data_aug.pt" if self.args.aug else "data.pt")
         self.field_text_file = os.path.join(args.data_dir, "field_text.pt")
+        self.field_char_file = os.path.join(args.data_dir, "field_char.pt")
 
         self.max_vocab_size = max_vocab_size
+        self.max_char_size = max_char_size
         self.min_freq = min_freq
         self.specials = specials
         # self.tokenizer = self.get_tokenizer()
 
         logger.info("Initial Corpus ...")
         self.field = {"text": TextField(tokenize_fn=None, special_tokens=self.specials),
+                      "char": TextField(tokenize_fn=lambda x: [y for y in x], special_tokens=self.specials),
                       "label": NumberField()}
 
         self.load()
@@ -107,7 +120,16 @@ class MatchCorpus(object):
                          "vocab_size": self.field["text"].vocab_size,
                          "specials": self.field["text"].specials
                          }
+        logger.info("Saved text field to '{}'".format(self.field_char_file))
+        field_char_article = {"itos": self.field["char"].itos,
+                         "stoi": self.field["char"].stoi,
+                         "vocab_size": self.field["char"].vocab_size,
+                         "specials": self.field["char"].specials
+                         }
+
+
         torch.save(field_article, self.field_text_file)
+        torch.save(field_char_article, self.field_char_file)
 
         logger.info("Saved data to '{}'".format(self.data_file))
         torch.save(self.data, self.data_file)
@@ -134,7 +156,8 @@ class MatchCorpus(object):
             line_items = [item for item in line.strip().split("\t") if item]
             text_a_tokens = self.tokenizer(line_items[1])
             text_b_tokens = self.tokenizer(line_items[2])
-            lines.append({"text_a": " ".join(text_a_tokens), "text_b": " ".join(text_b_tokens), "label": line_items[3]})
+            lines.append({"text_a": " ".join(text_a_tokens), "text_b": " ".join(text_b_tokens), "label": line_items[3],
+                          "char_a": "".join(text_a_tokens), "char_b": "".join(text_b_tokens)})
 
         logger.info("Read total {} examples from {}".format(len(lines), data_type.upper()))
         f.close()
@@ -151,6 +174,11 @@ class MatchCorpus(object):
                                        min_freq=self.min_freq,
                                        max_size=self.max_vocab_size)
 
+        cs = [[x["char_a"], x["char_b"]] for x in data]
+        self.field["char"].build_vocab(cs,
+                                       min_freq=self.min_freq,
+                                       max_size=self.max_char_size)
+
     def build_examples(self, data_raw, data_type="train"):
         if data_raw == None:
             logger.info("{} data text and label can't find".format(data_type))
@@ -162,6 +190,8 @@ class MatchCorpus(object):
             examples.append(Example(
                 text_a=data["text_a"],
                 text_b=data["text_b"],
+                char_a=data["char_a"],
+                char_b=data["char_b"],
                 label=data["label"])
             )
 
@@ -182,8 +212,9 @@ class MatchCorpus(object):
 
     def load_field(self):
         text_field = torch.load(self.field_text_file)
+        text_char_field = torch.load(self.field_char_file)
         self.field["text"].load(text_field)
-
+        self.field["char"].load(text_char_field)
     def create_batch(self, data_type="train"):
         examples = self.data[data_type]
         # FIXME Check example num
@@ -191,10 +222,15 @@ class MatchCorpus(object):
         features = self.convert_examples_to_features(examples)
         all_left_id = torch.tensor([f.left_ids for f in features], dtype=torch.long)
         all_right_id = torch.tensor([f.right_ids for f in features], dtype=torch.long)
+        all_left_char_id = torch.tensor([f.left_char_ids for f in features], dtype=torch.long)
+        all_right_char_id = torch.tensor([f.right_char_ids for f in features], dtype=torch.long)
         all_left_len = torch.tensor([f.left_len for f in features], dtype=torch.long)
         all_right_len = torch.tensor([f.right_len for f in features], dtype=torch.long)
+        all_left_char_len = torch.tensor([f.left_chars_len for f in features], dtype=torch.long)
+        all_right_char_len = torch.tensor([f.right_chars_len for f in features], dtype=torch.long)
         all_label = torch.tensor([f.label for f in features], dtype=torch.long)
-        dataset = TensorDataset(all_left_id, all_right_id, all_left_len, all_right_len, all_label)
+
+        dataset = TensorDataset(all_left_id, all_right_id, all_left_len, all_right_len, all_left_char_id, all_right_char_id, all_left_char_len, all_right_char_len, all_label)
 
         if data_type == "train":
             train_sampler = RandomSampler(dataset) if self.args.local_rank == -1 else DistributedSampler(dataset)
@@ -221,6 +257,7 @@ class MatchCorpus(object):
     def convert_examples_to_features(self, examples, data_type="train"):
         features = []
         text_a_len, text_b_len = [], []
+        chars_a_len, chars_b_len = [], []
         desc_message = "GETDATA FROM " + data_type.upper()
         for example in tqdm(examples, desc=desc_message):
             text_a_words = str.split(example.text_a)
@@ -236,38 +273,68 @@ class MatchCorpus(object):
                 text_b_words = text_b_words[:self.args.max_seq_length]
             right_len = len(text_b_words)
             left_ids, right_ids, left_char_ids, right_char_ids = [], [], [], []
+            char_a_len, char_b_len = [], []
             for i, word in enumerate(text_a_words):
                 left_ids.append(
                     self.field["text"].stoi.get(word, self.field["text"].stoi.get(constants.UNK_WORD)))
                 left_char_id = []
                 for char in word:
                     left_char_id.append(
-                        self.field["text_char"].stoi.get(char, self.field["text_char"].stoi.get(constants.UNK_WORD)))
+                        self.field["char"].stoi.get(char, self.field["char"].stoi.get(constants.UNK_WORD)))
+                chars_a_len.append(len(left_char_id))
+                if len(left_char_id) > self.args.max_char_seq_length:
+                    left_char_id = left_char_id[:self.args.max_char_seq_length]
                 left_char_ids.append(left_char_id)
+                char_a_len.append(len(left_char_id))
 
             for i, word in enumerate(text_b_words):
                 right_ids.append(
                     self.field["text"].stoi.get(word, self.field["text"].stoi.get(constants.UNK_WORD)))
-
+                right_char_id = []
+                for char in word:
+                    right_char_id.append(
+                        self.field["char"].stoi.get(char, self.field["char"].stoi.get(constants.UNK_WORD)))
+                chars_b_len.append(len(right_char_id))
+                if len(right_char_id) > self.args.max_char_seq_length:
+                    right_char_id = right_char_id[:self.args.max_char_seq_length]
+                right_char_ids.append(right_char_id)
+                char_b_len.append(len(right_char_id))
             padding_id = self.field["text"].stoi[constants.PAD_WORD]
             left_ids = self.padding_seq(left_ids, self.args.max_seq_length, padding_id)
             right_ids = self.padding_seq(right_ids, self.args.max_seq_length, padding_id)
 
+            char_padding_id = self.field["char"].stoi[constants.PAD_WORD]
+            left_char_ids = self.padding_char_seq(left_char_ids, self.args.max_seq_length, char_padding_id, self.args.max_char_seq_length)
+            right_char_ids = self.padding_char_seq(right_char_ids, self.args.max_seq_length, char_padding_id, self.args.max_char_seq_length)
+
+            char_a_len = self.padding_seq(char_a_len, self.args.max_seq_length, padding_id)
+            char_b_len = self.padding_seq(char_b_len, self.args.max_seq_length, padding_id)
+
             assert len(left_ids) == self.args.max_seq_length
             assert len(right_ids) == self.args.max_seq_length
+            assert len(left_char_ids) == self.args.max_seq_length
+            assert len(right_char_ids) == self.args.max_seq_length
 
             features.append(InputFeatures(
                 left_ids=left_ids,
                 right_ids=right_ids,
+                left_char_ids=left_char_ids,
+                right_char_ids=right_char_ids,
                 left_len=left_len,
                 right_len=right_len,
+                left_chars_len=char_a_len,
+                right_chars_len=char_b_len,
                 label=int(example.label)
             ))
 
         text_a_len = np.array(text_a_len)
         text_b_len = np.array(text_b_len)
+        chars_a_len = np.array(chars_a_len)
+        chars_b_len = np.array(chars_b_len)
         print("{} sequence length converge 95%".format(np.percentile(text_a_len, 95)))
         print("{} sequence length converge 95%".format(np.percentile(text_b_len, 95)))
+        print("{} char length converge 95%".format(np.percentile(chars_a_len, 95)))
+        print("{} char length converge 95%".format(np.percentile(chars_b_len, 95)))
         return features
 
     def padding_seq(self, seq, max_len, pad_id):
@@ -275,6 +342,18 @@ class MatchCorpus(object):
         seq += [pad_id] * padding_length
         return seq
 
+    def padding_char_seq(self, seq, max_len, pad_id, max_char_len):
+        for i in range(len(seq)):
+            padding_char_length = max_char_len - len(seq[i])
+            seq[i] += [pad_id] * padding_char_length
+        padding_length = max_len - len(seq)
+        seq += [[pad_id for i in range(max_char_len)]] * padding_length
+        return seq
+
+    def padding_char_len(self, seq, max_len, pad_id):
+        padding_length = max_len - len(seq)
+        seq += [pad_id] * padding_length
+        return seq
 
 if __name__ == "__main__":
     import argparse
